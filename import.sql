aafcffdc85d11682f8539b4f131b22a6c47944ee
AhmadRendi <ahmadrendiajah@gmail.com>
2025-09-02 17:10:37 +0800

    commit database

---
+++ import.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 9.4.0, for macos15.4 (arm64)
+--
+-- Host: localhost    Database: dba1
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_hobi`
+--
+
+DROP TABLE IF EXISTS `tbl_hobi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_hobi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name_hobi` varchar(76) NOT NULL,
+  `id_person` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_hobi_person` (`id_person`),
+  CONSTRAINT `fk_hobi_person` FOREIGN KEY (`id_person`) REFERENCES `tbl_person` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_hobi`
+--
+
+LOCK TABLES `tbl_hobi` WRITE;
+/*!40000 ALTER TABLE `tbl_hobi` DISABLE KEYS */;
+INSERT INTO `tbl_hobi` VALUES (1,'Futsal',1),(2,'Basket',2);
+/*!40000 ALTER TABLE `tbl_hobi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_person`
+--
+
+DROP TABLE IF EXISTS `tbl_person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_person` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_person`
+--
+
+LOCK TABLES `tbl_person` WRITE;
+/*!40000 ALTER TABLE `tbl_person` DISABLE KEYS */;
+INSERT INTO `tbl_person` VALUES (1,'Sentot','Aaa 11'),(2,'Anna','Bbb 21');
+/*!40000 ALTER TABLE `tbl_person` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-02 17:10:03
